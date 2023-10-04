1e44c094961e1cfbc1174e06db533d95e4a1a1b6
anithaamarnath <anithaamarnath04@gmail.com>
2023-10-04 04:07:22 -0700

    update the src file of redux

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,7 @@
 // CreateStore is the main method to create redux store
 // CombineReducers allows the user to combbine multiple reducers together
 import {configureStore} from '@reduxjs/toolkit'
-import authReducer from "../slices/authSlice";
+import authReducer from "../slices/AuthSlice";
 
 // Creating store
 let store = configureStore({
