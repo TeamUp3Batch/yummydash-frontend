fb83e76d7372f3dafa90a3a1e7916f47bb17328a
Kavitha Shanmugan <kavitha.shanmugan@gmail.com>
2023-10-05 00:07:15 -0600

    updated gitignore

--- src/components/LoginModal/LoginModal.jsx
+++ src/components/LoginModal/LoginModal.jsx
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 import AlertTitle from "@mui/material/AlertTitle";
 import Alert from "@mui/material/Alert";
 const LoginModal = ({ isOpen, onClose }) => {
+  const API_URL = process.env.REACT_APP_API_URL;
   const [data, setData] = useState({
     email: "",
     password: "",
@@ -18,9 +19,14 @@ const LoginModal = ({ isOpen, onClose }) => {
   };
 
   const handleSubmit = async (e) => {
+    console.log("HELLO ")
     e.preventDefault();
     try {
-      const url = "http://localhost:5000/api/auth/login";
+      console.log("api url",API_URL)
+      const url = API_URL+"/api/auth/login";
+      console.log("url is",url)
+      
+      // const url = "http://localhost:5000/api/auth/login";
       const result = await axios.post(url, data);
       if (result.data.status === "logged in") {
         sessionStorage.setItem("token", result.data.token);
