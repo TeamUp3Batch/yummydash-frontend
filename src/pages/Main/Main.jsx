7db1b260fab7c850e694bbb64244d87294b082dd
Kate <polyakovkate@gmail.com>
2023-10-05 20:56:14 -0700

    after main merge

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,14 +1,16 @@
-import React from "react";
-import Header from "../Header/Header";
-import Footer from "../../components/Footer/Footer";
-import CardCarousel from "../../components/Carousel/CardCarousel";
+import React from 'react';
+import Header from '../Header/Header';
+import Footer from '../../components/Footer/Footer';
+import CardCarousel from '../../components/Carousel/CardCarousel';
+import CarouselMain from '../../components/Carousel/Carousel';
 
 const Main = () => {
   return (
     <React.Fragment>
       <Header />
-      <div style={{ height: '800px' }}> 
-      {/* <CardCarousel/> */}
+      <div style={{ height: '800px' }}>
+        {/* <CardCarousel/> */}
+        <CarouselMain />
       </div>
       <Footer />
     </React.Fragment>
