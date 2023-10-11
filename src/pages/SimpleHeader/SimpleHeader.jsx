a538f5e927319dc7718d82d9f8732e3d435197c3
anithaamarnath <anithaamarnath04@gmail.com>
2023-10-10 17:05:59 -0700

    Removed Divider import

--- src/pages/SimpleHeader/SimpleHeader.jsx
+++ src/pages/SimpleHeader/SimpleHeader.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Button } from "@mui/material";
-import Divider from "@mui/material/Divider";
 import { Link } from "react-router-dom";
 import logo from "../../img/yummyDashLogo.png";
 import styles from "../SimpleHeader/simpleheader.module.scss";
