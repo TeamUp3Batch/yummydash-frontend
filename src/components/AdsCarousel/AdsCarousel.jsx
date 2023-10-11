b495586217be81a528d6157c4d49c77d1c498e1b
Kate <polyakovkate@gmail.com>
2023-10-11 00:06:09 -0700

    ready for merge

--- src/components/AdsCarousel/AdsCarousel.jsx
+++ src/components/AdsCarousel/AdsCarousel.jsx
@@ -10,39 +10,43 @@ import 'swiper/css/autoplay';
 
 import classes from './AdsCarousel.module.scss';
 
-import image1 from '../../img/carouselImage1.jpeg';
-import image2 from '../../img/carouselImage2.jpeg';
-import image3 from '../../img/carouselImage3.jpeg';
-import image4 from '../../img/carouselImage4.jpeg';
+import image1 from '../../img/yummyAdCarousel1.png';
+import image2 from '../../img/yummyAdCarousel2.png';
+import image3 from '../../img/yummyAdCarousel3.png';
+import image4 from '../../img/yummyAdCarousel4.png';
 
 const AdsCarousel = () => {
   return (
     <div className={classes.adsswiper__wrapper}>
-      <Swiper modules={[Navigation, Autoplay]} spaceBetween={14} slidesPerView={2} navigation autoplay>
+      <Swiper
+        modules={[Navigation, Autoplay]}
+        spaceBetween={14}
+        slidesPerView={2}
+        navigation
+        autoplay>
         <SwiperSlide>
           <div className={classes.swiper__card}>
-            <img src={image1} alt="clone"/>
+            <img src={image1} alt="clone" />
           </div>
         </SwiperSlide>
 
         <SwiperSlide>
           <div className={classes.swiper__card}>
-            <img src={image2} alt="clone"/>
+            <img src={image2} alt="clone" />
           </div>
         </SwiperSlide>
 
         <SwiperSlide>
           <div className={classes.swiper__card}>
-            <img src={image3} alt="clone"/>
+            <img src={image3} alt="clone" />
           </div>
         </SwiperSlide>
 
         <SwiperSlide>
           <div className={classes.swiper__card}>
-            <img src={image4} alt="clone"/>
+            <img src={image4} alt="clone" />
           </div>
         </SwiperSlide>
-        
       </Swiper>
     </div>
   );
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -2,17 +2,16 @@ import React from 'react';
 import Header from '../Header/Header';
 import Footer from '../../components/Footer/Footer';
 
-import CarouselMain from '../../components/Carousel/Carousel';
 import AdsCarousel from '../../components/AdsCarousel/AdsCarousel';
+import CuisineCarousel from '../../components/CuisineCarousel/CuisineCarousel';
 
 const Main = () => {
   return (
     <React.Fragment>
       <Header />
       <div style={{ height: '800px' }}>
-        {/* <CardCarousel/> */}
         <AdsCarousel />
-        <CarouselMain />
+        <CuisineCarousel />
       </div>
       <Footer />
     </React.Fragment>
