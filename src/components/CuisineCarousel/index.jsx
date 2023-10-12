b252187d9db90b9ed20400ec7b3b71a253d564a8
Kavitha Shanmugan <kavitha.shanmugan@gmail.com>
2023-10-12 13:39:30 -0600

    added fixes

--- src/components/CuisineCarousel/index.jsx
+++ src/components/CuisineCarousel/index.jsx
@@ -9,7 +9,7 @@ import { CuisinesURL } from "../../api/ApiLinks";
 // import Swiper core and required modules
 import { Navigation } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
-
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/navigation";
@@ -44,11 +44,23 @@ const MyComponent = () => {
                 className={classes.swiper__card}
                 key={card._id}
                 style={{
-                  cursor:'pointer',
+                  cursor: "pointer",
                   background: `linear-gradient(rgba(255, 255, 255, 0) 15%, rgba(0, 0, 0, 0.6) 100%), url(${card.imageUrl})`,
                 }}
                 onClick={() => handleCuisineClick(card.name)}
               >
+                {selectedCuisine === card.name && (
+                  <CheckCircleIcon
+                    sx={{
+                      color: "red",
+                      position: "absolute",
+                      top: "10px",
+                      right: "10px",
+                      fontSize: "24px",
+                      zIndex: 1,
+                    }}
+                  />
+                )}
                 <p>{card.name}</p>
               </div>
             </SwiperSlide>
