67d3e28d462d3544dda6e01524537ef952061021
Kate <polyakovkate@gmail.com>
2023-10-11 17:24:03 -0700

    done

---
+++ src/api/ApiLinks.jsx
@@ -0,0 +1,2 @@
+
+export const CuisinesURL = "http://localhost:5000/api/cuisines/getAllCuisines";
--- src/components/CuisineCarousel/index.jsx
+++ src/components/CuisineCarousel/index.jsx
@@ -1,7 +1,12 @@
+import React from 'react';
+import { useEffect, useState } from 'react';
+
+import axios from 'axios';
+import { CuisinesURL } from '../../api/ApiLinks';
+
 // Import Swiper React components
 // import Swiper core and required modules
 import { Navigation } from 'swiper';
-
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 // Import Swiper styles
@@ -11,39 +16,27 @@ import 'swiper/css/navigation';
 import classes from './swiper.module.scss';
 
 const MyComponent = () => {
+  const [cuisineList, setCuisineList] = useState([]);
+
+  useEffect(() => {
+    axios.get(CuisinesURL).then((response) => setCuisineList(response.data));
+  }, []);
+
   return (
     <div className={classes.swiper__wrapper}>
       <Swiper modules={[Navigation]} spaceBetween={8} slidesPerView={4} navigation>
-        <SwiperSlide>
-          <div className={classes.swiper__card}>
-            <p>card name</p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className={classes.swiper__card}>
-            <p>card name</p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className={classes.swiper__card}>
-            <p>card name</p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className={classes.swiper__card}>
-            <p>card name</p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className={classes.swiper__card}>
-            <p>card name</p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className={classes.swiper__card}>
-            <p>card name</p>
-          </div>
-        </SwiperSlide>
+        {cuisineList.map((card) => (
+          <SwiperSlide>
+            <div
+              className={classes.swiper__card}
+              key={card._id}
+              style={{
+                background: `linear-gradient(rgba(255, 255, 255, 0) 15%, rgba(0, 0, 0, 0.6) 100%), url(${card.imageUrl})`,
+              }}>
+              <p>{card.name}</p>
+            </div>
+          </SwiperSlide>
+        ))}
       </Swiper>
     </div>
   );
