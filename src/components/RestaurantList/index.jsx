1e1082d83066157b5975abed460656c767bd9899
Kavitha Shanmugan <kavitha.shanmugan@gmail.com>
2023-10-14 23:29:07 -0600

    defined hooks for gettingRestaurantsByCuisine

--- src/components/RestaurantList/index.jsx
+++ src/components/RestaurantList/index.jsx
@@ -1,27 +1,30 @@
-import React, { useEffect, useState } from 'react';
-import RestaurantCard from '../RestaurantCard';
-import { getRestaurantsByCuisine } from '../../services/restaurantService'; // Import the service
-import { useDispatch } from 'react-redux';
-import { setRestaurantsByCuisine } from '../../slices/restaurantSlice';
+import React, { useEffect, useState } from "react";
+import RestaurantCard from "../RestaurantCard";
+import { getRestaurantsByCuisine } from "../../services/restaurantService"; // Import the service
+import { useDispatch } from "react-redux";
+import { setRestaurantsByCuisine } from "../../slices/restaurantSlice";
+import { useFetchRestaurants } from "./hooks/useFetchRestaurants";
 
 const RestaurantList = ({ selectedCuisine }) => {
-  const [restaurants, setRestaurants] = useState([]);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const data = await getRestaurantsByCuisine(selectedCuisine);
-        dispatch(setRestaurantsByCuisine());
-        setRestaurants(data);
-      } catch (error) {
-        console.error('Error:', error);
-      }
-    };
-
-    fetchData();
-  }, [selectedCuisine]);
-
+  const { restaurants, isLoading, isError } = useFetchRestaurants({
+    selectedCuisine,
+  });
+  if (isLoading) {
+    return (
+      <div>
+        <h1>Restaurants Near You</h1>
+        <div>Loading...</div>
+      </div>
+    );
+  }
+  if (isError) {
+    return (
+      <div>
+        <h1>Restaurants Near You</h1>
+        <div>Something went wrong</div>
+      </div>
+    );
+  }
   return (
     <div>
       <h1>Restaurants Near You</h1>
