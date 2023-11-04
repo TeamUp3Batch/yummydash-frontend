197ac9835a431be1809dabbf17e87aef5ee35c62
Kavitha Shanmugan <kavitha.shanmugan@gmail.com>
2023-11-04 13:01:10 -0600

    removed orders code

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import SignUp from "../src/pages/SignUp/SignUp";
 import Landing from "../src/pages/LandingPage/LandingPage";
 import Main from "../src/pages/Main/Main";
 import Profile from "./pages/ViewAccount/Profile";
-import PlaceOrder from "../src/pages/PlaceOrder/PlaceOrder";
 import MenuPage from "./pages/MenuPage/MenuPage";
-import DeliveryPage from "./pages/DeliveryPage/DeliveryPage";
 
 import { Routes, Route } from "react-router-dom";
 
@@ -15,9 +13,7 @@ function App() {
       <Route path="/signup" element={<SignUp />} />
       <Route path="/main" element={<Main />} />
       <Route path="/profile" element={<Profile />} />
-      <Route path="/order" element={<PlaceOrder />} />
       <Route path="/menu/:restaurantId" element={<MenuPage />} />
-      <Route path="/delivery" element={<DeliveryPage />} />
     </Routes>
   );
 }
