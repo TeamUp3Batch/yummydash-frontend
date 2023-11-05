6b93e61562ac0ff438c66c49b97981a7f514f0c9
Kate <polyakovkate@gmail.com>
2023-11-04 23:39:05 -0700

    done with dish modile view

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
 
 import Modal from './Modal/Modal';
+import DishModal from './DishModal/DishModal';
+
 import Star from '../../icons/star-svgrepo-com.svg';
 import Info from '../../icons/info-circle-svgrepo-com.svg';
 import classes from './menu.module.scss';
 
 const Menu = ({ restaurantDetails }) => {
   const [modalActive, setModalActive] = useState(false);
+  const [dishModalActive, setDishModalActive] = useState(false);
   const [searchValue, setSearchValue] = useState('');
 
   const onChangeSearchValue = (event) => {
@@ -78,7 +81,7 @@ const Menu = ({ restaurantDetails }) => {
             {restaurantDetails.menu
               .filter((type) => type.name.toLowerCase().includes(searchValue.toLowerCase()))
               .map((type) => (
-                <button key={type._id} className={classes.restauranMenu__dishes__wrapper}>
+                <button key={type._id} className={classes.restauranMenu__dishes__wrapper} onClick={() => setDishModalActive(true)}>
                   <div className={classes.restauranMenu__dishes__inside}>
                     <p>Category: {type.category}</p>
                     <div>
@@ -94,6 +97,7 @@ const Menu = ({ restaurantDetails }) => {
       </div>
 
       <Modal active={modalActive} setActive={setModalActive} />
+      <DishModal active={dishModalActive} setActive={setDishModalActive} />
     </div>
   );
 };
