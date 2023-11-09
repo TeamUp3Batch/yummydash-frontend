42df83b31c44b7d78c3b7b7b356a2b1ddeadbed9
Kate <polyakovkate@gmail.com>
2023-11-08 19:59:54 -0800

    fixed bugs

--- src/components/Menu/DishModal/DishModal.jsx
+++ src/components/Menu/DishModal/DishModal.jsx
@@ -4,13 +4,15 @@ import PlusIcon from '../../../icons/icons8-plus-25.png';
 import MinusIcon from '../../../icons/icons8-minus-25.png';
 import classes from './dishModal.module.scss';
 import { useAddToCartHooks } from '../hooks/useAddCartHooks';
-import { useDispatch, useSelector } from "react-redux";
-
-
+import { useDispatch, useSelector } from 'react-redux';
 
 const DishModal = ({ active, setActive, restaurantId, menuItem }) => {
   const { count, increment, decrement, addedToCart } = useAddToCartHooks(restaurantId, menuItem);
 
+  const onClickButton = () => {
+    addedToCart();
+    setActive(false);
+  };
 
   return (
     <div className={active ? classes.dishModal__active : classes.dishModal}>
@@ -38,9 +40,9 @@ const DishModal = ({ active, setActive, restaurantId, menuItem }) => {
           </div>
           <div className={classes.dishModal__inside__footer}>
             <div className={classes.dishModal__footer__button}>
-              <button onClick={addedToCart}>
+              <button onClick={onClickButton}>
                 <p>Add To Cart</p>
-                <p>$ {(count * (menuItem ? menuItem.price : 0))}</p>
+                <p>$ {count * (menuItem ? menuItem.price : 0)}</p>
               </button>
             </div>
           </div>
