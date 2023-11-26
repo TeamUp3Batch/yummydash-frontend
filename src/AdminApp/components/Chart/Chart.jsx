dc49b54988b2e41fc710183bb9bf7f3691541c07
Kate <polyakovkate@gmail.com>
2023-11-25 22:24:37 -0800

    added white header

--- src/AdminApp/components/Chart/Chart.jsx
+++ src/AdminApp/components/Chart/Chart.jsx
@@ -12,7 +12,6 @@ const COLORS = ['#0088FE', '#00C49F', '#FFBB28'];
 export default function Chart() {
   return (
     <PieChart width={300} height={200}>
-
       <Pie
         data={data}
         cx="50%"
@@ -21,8 +20,7 @@ export default function Chart() {
         outerRadius={80}
         fill="#8884d8"
         paddingAngle={5}
-        dataKey="value"
-      >
+        dataKey="value">
         {data.map((entry, index) => (
           <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
         ))}
