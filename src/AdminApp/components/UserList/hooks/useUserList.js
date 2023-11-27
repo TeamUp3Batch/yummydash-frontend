2c34927a5bce95a143434d426879570ecb19bc5b
anithaamarnath <anithaamarnath04@gmail.com>
2023-11-26 20:58:01 -0800

    Added all user edit action

--- src/AdminApp/components/UserList/hooks/useUserList.js
+++ src/AdminApp/components/UserList/hooks/useUserList.js
@@ -1,12 +1,27 @@
 import { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
-import { getAllUsers } from "../../../../services/userService";
+
+import { updateUserProfile, getAllUsers } from "../../../../services/userService"; 
 
 export const useUserList = () => {
   const [userList, setUserList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState();
-  const dispatch = useDispatch();
+  const [editingUserId, setEditingUserId] = useState(null);
+  const [showEditForm, setShowEditForm] = useState(false);
+  const [userData, setUserData] = useState({
+    firstName: "",
+    lastName: "",
+    phoneNumber: null,
+    email: "",
+    userId: "",
+  });
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setUserData({
+      ...userData,
+      [name]: value,
+    });
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -26,9 +41,61 @@ export const useUserList = () => {
     fetchData();
   }, []);
 
+  const handleSaveEdit = async () => {
+    try {
+      const editedUser = await updateUserProfile(userData);
+
+      if (editedUser) {
+        const users = await getAllUsers();
+        setUserList(users);
+      } else {
+        console.error("Error editing User.");
+      }
+    } catch (error) {
+      console.error("Error editing User:", error);
+    }
+
+    resetForm();
+    setShowEditForm(false);
+  };
+
+  const handleEditUser =(userId) =>{
+    const userToEdit = userList.find((user) => user._id === userId);
+    setUserData({
+      firstName: userToEdit.firstName,
+      lastName: userToEdit.lastName,
+      phoneNumber: userToEdit.phoneNumber,
+      email: userToEdit.email,
+      userId: userId,
+    })
+    setEditingUserId(userId);
+    setShowEditForm(true);
+
+  }
+  const handleCancel = () => {
+    resetForm();
+    setShowEditForm(false);
+  };
+  const resetForm = () => {
+    setUserData({
+      firstName: "",
+    lastName: "",
+    phoneNumber: 0,
+    email: "",
+    userId: "",
+    });
+  };
+
   return {
     userList,
     isLoading,
-    isError
+    isError,
+    showEditForm,
+    userData,
+    handleEditUser,
+    setShowEditForm,
+    handleSaveEdit,
+    handleCancel,
+    handleChange
   };
 };
