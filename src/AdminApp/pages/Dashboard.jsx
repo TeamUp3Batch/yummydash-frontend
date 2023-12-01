a7168bae8516d5ebeb52374eadca537cd7440b0b
anithaamarnath <anithaamarnath04@gmail.com>
2023-11-30 17:32:38 -0800

    dashboard UI

--- src/AdminApp/pages/Dashboard.jsx
+++ src/AdminApp/pages/Dashboard.jsx
@@ -1,33 +1,33 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import TotalView from '../components/TotalView/TotalView';
-import Statistics from '../components/Statistics/Statistics';
-import TableView from '../components/TableView/TableView';
-import Chart from '../components/Chart/Chart';
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Grid from "@mui/material/Grid";
+import Statistics from "../components/Statistics/TotalCounts";
+import WeeklyChart from "../components/Chart/weeklyAreaChart";
+import MonthlyChart from "../components/Chart/monthlyAreaChart";
+import TopPerformance from "../components/TopPerformance/TopPerformance";
 
 const StyledGrid = styled(Grid)(({ theme }) => ({
   padding: theme.spacing(2),
+  height: "100%",
 }));
 
 export default function Dashboard() {
   return (
-    <Box>
-      <Grid container spacing={2}>
-        <StyledGrid item xs={12} sm={6}>
-          <TotalView />
-        </StyledGrid>
-        <StyledGrid item xs={12} sm={6}>
+    <>
+      <StyledGrid container spacing={2}>
+        <StyledGrid item xs={12}>
           <Statistics />
         </StyledGrid>
-        <StyledGrid item xs={12} sm={8}>
-          <TableView />
+        <StyledGrid item xs={12} sm={6}>
+          <WeeklyChart />
         </StyledGrid>
-        <StyledGrid item xs={12} sm={4}>
-          <Chart />
+        <StyledGrid item xs={12} sm={6}>
+          <MonthlyChart />
         </StyledGrid>
-      </Grid>
-    </Box>
+        <StyledGrid item xs={12}>
+          <TopPerformance />
+        </StyledGrid>
+      </StyledGrid>
+    </>
   );
 }
